53601fcf0652e711d49816dbc39b6c3e76073784
Andrew Chung <aandrewchung@gmail.com>
2024-04-04 10:12:13 -0400

    created pages dir n home file

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+//HomePage.js
+import React from 'react';
+import '../assets/styles/HomePage.css'; 
+
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+    </div>
+  );
+}
+
+export default HomePage;
